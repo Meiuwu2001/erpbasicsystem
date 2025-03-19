b1675fdf6d6ce20ee567646a9dd648501ea082c0
Samuel Reyna González <118919573+Meiuwu2001@users.noreply.github.com>
2025-03-18 19:11:54 -0600

    Refactor login component: rename password to contrasenia and update login logic to handle API response

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -12,13 +12,34 @@ const Login: React.FC = () => {
     }
   }, [router]);
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [contrasenia, setcontrasenia] = useState("");
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     // Handle login logic here
     console.log("Email:", email);
-    console.log("Password:", password);
+    console.log("contrasenia:", contrasenia);
+
+    fetch("/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, contrasenia }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        localStorage.setItem("token", data.token);
+        if (data.token) {
+          router.push("/dashboard");
+        } else {
+          alert("Invalid credentials");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
   return (
@@ -44,16 +65,16 @@ const Login: React.FC = () => {
           </div>
           <div>
             <label
-              htmlFor="password"
+              htmlFor="contrasenia"
               className="block text-sm font-medium text-gray-700"
             >
-              Password:
+              contrasenia:
             </label>
             <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              type="contrasenia"
+              id="contrasenia"
+              value={contrasenia}
+              onChange={(e) => setcontrasenia(e.target.value)}
               required
               className="w-full px-3 py-2 mt-1 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
             />
